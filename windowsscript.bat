d4d9fbbe0f7c8dcf2d663b8f25450c50adaa160e
veljkoselakovic <bg.selakovicveljko@gmail.com>
2020-11-07 19:23:37 +0100

    Automatic commit

---
+++ windowsscript.bat
@@ -0,0 +1,9 @@
+cd C:\Users\ljove\downloads
+move OOP1.pdf C:\Users\ljove\Documents\GitHub\veljkoselakovic.github.io\OO1
+cd C:\Users\ljove\Documents\GitHub\veljkoselakovic.github.io\OO1
+del /f OO1.pdf
+ren OOP1.pdf OO1.pdf
+cd..
+git add .
+git commit -m "Automatic commit"
+git push
